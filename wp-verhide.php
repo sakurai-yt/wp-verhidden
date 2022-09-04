e44ff9b2892bebed6296fd8f06a7bda99ff16e8d
“Yuta <vantage-remark.07@icloud.com>
2022-09-04 10:15:33 +0900

    v1.0 微調整

--- wp-verhide.php
+++ wp-verhide.php
@@ -9,21 +9,22 @@ License: GPLv3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 
-remove_action('wp_head','wp_generator');
+remove_action('wp_head', 'wp_generator');
 
-function remove_cssjs_ver2( $src ) {
-    if ( strpos( $src, 'ver=' ) )
-        $src = remove_query_arg( 'ver', $src );
+function remove_cssjs_ver2($src)
+{
+    if (strpos($src, 'ver='))
+        $src = remove_query_arg('ver', $src);
     return $src;
 }
-add_filter( 'style_loader_src', 'remove_cssjs_ver2', 9999 );
-add_filter( 'script_loader_src', 'remove_cssjs_ver2', 9999 );
+add_filter('style_loader_src', 'remove_cssjs_ver2', 9999);
+add_filter('script_loader_src', 'remove_cssjs_ver2', 9999);
 
 require 'update/plugin-update-checker.php';
 $ExampleUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://www.skri.gr.jp/software/update/wp-verhide.json',
-	__FILE__,
-	'wp-verhide'
+    'https://www.skri.gr.jp/software/update/wp-verhide.json',
+    __FILE__,
+    'wp-verhide'
 );
